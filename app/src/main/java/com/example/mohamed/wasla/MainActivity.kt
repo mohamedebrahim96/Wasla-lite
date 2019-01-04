cd3c2d622ed4b318a5f83d24c08163f535e92a3b
Mohamed Ebrahim <ebrahimm131@gmail.com>
2019-01-04 02:26:55 +0200

    Main activity enhancement
    - remove toolbar
    - add title 
    - make status bar transparent

--- app/src/main/java/com/example/mohamed/wasla/MainActivity.kt
+++ app/src/main/java/com/example/mohamed/wasla/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.mohamed.wasla
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.os.Build
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupContentWindow()
         setContentView(R.layout.activity_main)
 
+    }
 
+    fun setupContentWindow() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.TRANSPARENT
+        }
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
